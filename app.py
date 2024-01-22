068f113531c1f32efbf2011143abdf9c7406ca03
Ahmad Didik Setiyadi <adidiksetiyadi@gmail.com>
2024-01-23 05:23:45 +0700

    add abort and fix store_id typo

--- app.py
+++ app.py
@@ -1,49 +1,49 @@
 import uuid
 from flask import Flask
 from flask import request
+from flask_smorest import abort
 from db import stores, items
 
 app = Flask(__name__)
 
-@app.get("/stores")
+@app.get("/store")
 def get_stores():
   return {"stores": list(stores.values())}
-
-
-@app.post("/store")
-def create_store():
-  store_data = request.get_json()
-  store_id = uuid.uuid4().hex
-  new_store = {**store_data, "id": store_id}
-  stores[id] = new_store
-  return new_store, 201
-
-@app.post("/item")
-def create_item(name):
-  item_data = request.get_json()
-  if item_data["store_id"] not in stores:
-    return {"message": "Store not found"}, 404
-  
-  item_id = uuid.uuid4().hex
-  item = {**item_data, "id":item_id}
-  items[item_id] = item
-  
-  return item, 201
-
-@app.get("/item")
-def get_all_items():
-  return {"items": list(items.values())}
 
 @app.get("/store/<string:store_id>")
 def get_store(store_id):
   try:
     return stores[store_id]
   except KeyError:
-    return {"message": "Store not found"}, 404
+    abort(404, message="Store not found.")
+
+@app.post("/store")
+def create_store():
+  store_data = request.get_json()
+  store_id = uuid.uuid4().hex
+  store = {**store_data, "id": store_id}
+  stores[store_id] = store
+  return store, 201
+
+@app.get("/item")
+def get_items():
+  return {"items": list(items.values())}
 
 @app.get("/item/<string:item_id>")
 def get_item(item_id):
   try:
     return items[item_id]
   except KeyError:
-    return {"message": "Item not found"}, 404
+    abort(404, message="Items not found.")
+
+@app.post("/item")
+def create_item(name):
+  item_data = request.get_json()
+  if item_data["store_id"] not in stores:
+    abort(404, message="Store not found.")
+  
+  item_id = uuid.uuid4().hex
+  item = {**item_data, "id":item_id}
+  items[item_id] = item
+  
+  return item, 201
