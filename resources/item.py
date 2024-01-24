619e68a5f333847c19bf43b0b0578b1c91e13152
Ahmad Didik Setiyadi <adidiksetiyadi@gmail.com>
2024-01-24 17:06:58 +0700

    configure sqlalchemy

--- resources/item.py
+++ resources/item.py
@@ -1,9 +1,10 @@
-import uuid
-from flask import request
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
-from db import items
-from schemas import ItemSchema, ItemUpdateSchema
+from sqlalchemy.exc import SQLAlchemyError
+
+from db import db
+from models import itemModel
+from schemas import ItemSchema, PlainItemSchema
 
 blp = Blueprint("Items", __name__, description="Operations on items")
 
@@ -11,49 +12,23 @@ blp = Blueprint("Items", __name__, description="Operations on items")
 class Items(MethodView):
   @blp.response(200, ItemSchema)
   def get(self, item_id):
-    try:
-      return items[item_id]
-    except KeyError:
-      abort(404, message="Items not found.")
+    raise NotImplementedError("Getting an item is not implemented.")
   
-  @blp.arguments(ItemUpdateSchema)
+  @blp.arguments(PlainItemSchema)
   @blp.response(200, ItemSchema)
   def put(self, item_data, item_id):
-    try:
-      item = items[item_id]
-      item |= item_data
-      return item
-    except KeyError:
-      abort(400, message="Item not found")
+   raise NotImplementedError("Updating an item is not implemented.")
   
   def delete(self, item_id):
-    try:
-      del items[item_id]
-      return {"message": "Item deleted."}
-    except KeyError:
-      abort(400, message="Item not found.")
+    raise NotImplementedError("Deleting an item is not implemented.")
 
 @blp.route("/item")
 class ItemList(MethodView):
   @blp.response(200, ItemSchema(many=True))
   def get(self):
-    return items.values()
+    raise NotImplementedError("Listing items is not implemented.")
 
   @blp.arguments(ItemSchema)
   @blp.response(201, ItemSchema)
   def post(self, item_data):
-    for item in items.values():
-      if(
-        item_data["name"] == item["name"]
-        and item_data["store_id"] == item["store_id"]
-      ):
-        abort(400, message="Item already exist.")
-        
-    if item_data["store_id"] not in stores:
-      abort(404, message="Store not found.")
-    
-    item_id = uuid.uuid4().hex
-    item = {**item_data, "id":item_id}
-    items[item_id] = item
-    
-    return item
+    raise NotImplementedError("Creating an item is not implemented.")
--- resources/store.py
+++ resources/store.py
@@ -1,8 +1,9 @@
-import uuid
-from flask import request
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
-from db import stores
+from sqlalchemy.exc import SQLAlchemyError, IntegrityError
+
+from db import db
+from models import StoreModel
 from schemas import StoreSchema
 
 blp = Blueprint("Stores", __name__, description="Operations on stores")
@@ -11,42 +12,22 @@ blp = Blueprint("Stores", __name__, description="Operations on stores")
 class Store(MethodView):
   @blp.response(200, StoreSchema)
   def get(self, store_id):
-    try:
-      return stores[store_id]
-    except KeyError:
-      abort(404, message="Store not found.")
+     raise NotImplementedError("Getting a store is not implemented.")
 
   @blp.arguments(StoreSchema)
   def put(self, store_data, store_id):
-    try:
-      store = stores[store_id]
-      store |= store_data
-      return store
-    except KeyError:
-      abort(400, message="Item not found")
+     raise NotImplementedError("Updating a store is not implemented.")
   
   def delete(self, store_id):
-    try:
-      del stores[store_id]
-      return {"message": "Store deleted."}
-    except KeyError:
-      abort(400, message="Store not found.")
+     raise NotImplementedError("Deleting a store is not implemented.")
 
 @blp.route("/store")
 class StoreList(MethodView):
   @blp.response(200, StoreSchema(many=True))
   def get(self):
-    return stores.values()
+    raise NotImplementedError("Listing stores is not implemented.")
     
   @blp.arguments(StoreSchema)
   @blp.response(201, StoreSchema)
   def post(self, store_data):
-    for store in stores.values():
-      if store_data["name"] == store["name"]:
-        abort(400, message="Store already exist.")
-
-    store_id = uuid.uuid4().hex
-    store = {**store_data, "id": store_id}
-    stores[store_id] = store
-    print(stores)
-    return store
+    raise NotImplementedError("Creating a store is not implemented.")
